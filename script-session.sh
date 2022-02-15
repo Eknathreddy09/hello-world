e036cab6ce5067c7fc44040bda0cd12964b86e4f
Eknathreddy09 <eknath.reddy09@gmail.com>
2022-02-15 11:43:24 +0530

    Update script-session.sh

--- script-session.sh
+++ script-session.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 kubectl config delete-context eduk8s
 mv ~/config ~/.kube/config
-tanzu login --kubeconfig ~/.kube/config --context tkg-mgmttest-cluster-admin@tkg-mgmttest-cluster --name tkg-mgmttest-cluster
-kubectl config set-context tkg-mgmttest-cluster-admin@tkg-mgmttest-cluster
-kubectl config use-context tkg-mgmttest-cluster-admin@tkg-mgmttest-cluster
+tanzu login --kubeconfig ~/.kube/config --context partnerse-demo-mgmt-admin@partnerse-demo-mgmt --name partnerse-demo-mgmt
+kubectl config set-context partnerse-demo-mgmt-admin@partnerse-demo-mgmt
+kubectl config use-context partnerse-demo-mgmt-admin@partnerse-demo-mgmt
 kubectl config get-contexts
 tanzu mc get
