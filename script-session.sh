cd2bc97ea427e8671028b5d641a228ce152f28fe
Eknathreddy09 <eknath.reddy09@gmail.com>
2022-02-16 10:17:35 +0530

    Update script-session.sh

--- script-session.sh
+++ script-session.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 kubectl config delete-context eduk8s
 mv ~/config ~/.kube/config
+mv ~/jb_instance ~/.kube/jb_instance
 tanzu login --kubeconfig ~/.kube/config --context partnerse-demo-mgmt-admin@partnerse-demo-mgmt --name partnerse-demo-mgmt
 kubectl config set-context partnerse-demo-mgmt-admin@partnerse-demo-mgmt
 kubectl config use-context partnerse-demo-mgmt-admin@partnerse-demo-mgmt
