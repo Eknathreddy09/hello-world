1ad2a22dee90bf90b65c121993be2d60a5b9f293
Eknathreddy09 <eknath.reddy09@gmail.com>
2022-02-17 15:31:06 +0530

    Update script-session-tmc.sh

--- script-session-tmc.sh
+++ script-session-tmc.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 scp -i ~/id_rsa -o StrictHostKeyChecking=accept-new azureuser@52.140.0.80:~/.kube/config ~/.kube/config
-#tanzu login --kubeconfig ~/.kube/config --context partnerse-demo-mgmt-admin@partnerse-demo-mgmt --name partnerse-demo-mgmt
-#kubectl config set-context partnerse-demo-mgmt-admin@partnerse-demo-mgmt
-#kubectl config use-context partnerse-demo-mgmt-admin@partnerse-demo-mgmt
-#kubectl config get-contexts
-#tanzu mc get
+username=$(kubectl config view -o jsonpath={.users[0].name})
+clustername=$(kubectl config view -o jsonpath={.contexts[0].context.cluster})
+tanzu login --kubeconfig ~/.kube/config --context $username@$clustername --name $clustername
+kubectl config set-context $username@$clustername
+kubectl config use-context $username@$clustername
