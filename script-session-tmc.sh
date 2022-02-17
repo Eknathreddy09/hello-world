78c405e8b94512618ff724cfbeca76469822982f
Eknathreddy09 <eknath.reddy09@gmail.com>
2022-02-17 16:00:31 +0530

    Update script-session-tmc.sh

--- script-session-tmc.sh
+++ script-session-tmc.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-scp -i ~/id_rsa -o StrictHostKeyChecking=accept-new azureuser@VMIP:~/.kube/config ~/.kube/config
+scp -i ~/id_rsa -o StrictHostKeyChecking=accept-new azureuser@$VMIP:~/.kube/config ~/.kube/config
 username=$(kubectl config view -o jsonpath={.users[0].name})
 clustername=$(kubectl config view -o jsonpath={.contexts[0].context.cluster})
 tanzu login --kubeconfig ~/.kube/config --context $username@$clustername --name $clustername
