c4a3c65d27230bfea55e5f2beca7e703b78baf82
Eknathreddy09 <eknath.reddy09@gmail.com>
2022-02-17 11:54:34 +0530

    Update script-session-tmc.sh

--- script-session-tmc.sh
+++ script-session-tmc.sh
@@ -1,6 +1,6 @@
 ### To be changed - IP
 
-scp -i ~/id_rsa -o StrictHostKeyChecking=accept-new azureuser@52.140.0.80:~/.kube/config ~/.kube 
+scp -i ~/id_rsa -o StrictHostKeyChecking=accept-new azureuser@52.140.0.80:~/.kube-tkg/config ~/.kube/config
 #tanzu login --kubeconfig ~/.kube/config --context partnerse-demo-mgmt-admin@partnerse-demo-mgmt --name partnerse-demo-mgmt
 #kubectl config set-context partnerse-demo-mgmt-admin@partnerse-demo-mgmt
 #kubectl config use-context partnerse-demo-mgmt-admin@partnerse-demo-mgmt
