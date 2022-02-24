a0acbe3054a771addd04448f9b39daf8dab46350
Eknathreddy09 <eknath.reddy09@gmail.com>
2022-02-24 20:34:09 +0530

    Update script-session.sh

--- script-session.sh
+++ script-session.sh
@@ -6,7 +6,7 @@ curl https://raw.githubusercontent.com/Eknathreddy09/hello-world/master/jb_insta
 chmod 700 ~/id_rsa
 azportallogin=$(az login --service-principal -u c35f2d8b-1e51-4aba-ac3e-f9135baf97c1 -p --tenant b39138ca-3cee-4b4a-a4d6-cd83d9dd62f0)
 azgroupcreate=$(az group create -l centralindia -n $SESSION_NAME-JB)
-vmcreate=$(az vm create -g $SESSION_NAME-JB -n $SESSION_NAME --image /subscriptions/827ba431-f0b2-4e6b-aa9b-45d645395244/resourceGroups/images-rg/providers/Microsoft.Compute/images/test-image-image-20220222162601 --size Standard_DS2_v2 --ssh-key-values id_rsa.pub)
+vmcreate=$(az vm create -g $SESSION_NAME-JB -n $SESSION_NAME --image /subscriptions/827ba431-f0b2-4e6b-aa9b-45d645395244/resourceGroups/images-rg/providers/Microsoft.Compute/images/test-image-image-20220222162601 --size Standard_DS2_v2 --public-ip-address-dns-name $SESSION_NAME --ssh-key-values id_rsa.pub)
 VMIPs=$(az vm list-ip-addresses --resource-group $SESSION_NAME-JB --name $SESSION_NAME --query "[].virtualMachine.network.publicIpAddresses[0].ipAddress" --output tsv)
 export VMIP=$VMIPs
 az vm open-port --resource-group $SESSION_NAME-JB --name $SESSION_NAME --port 8080
